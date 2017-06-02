ddee47c85c2c390c1790f37927ff95fbdf4301e7
FreshOceans <mperezz398@gmail.com>
2017-06-02 15:31:01 -0400

    finished core assets

--- scripts.js
+++ scripts.js
@@ -1,22 +1,18 @@
 window.addEventListener("load", showModalDiv);
+var modalEl = document.getElementById("primeModal");
 
 function showModalDiv(){
   console.log("== showModalDiv ==");
-  var modalEl = document.getElementById("primeModal");
-  console.log("displayModal:", modalEl);
-  setTimeout(function(){modalEl.style.display = "block"}, 3000);
+  setTimeout(function(){
+    modalEl.style.display = "block"}, 3000);
+  var exitEl = document.getElementById("exit");
+  exitEl.addEventListener("click", hideModalDiv);
+}
+function hideModalDiv(){
+  console.log("== hideModalDiv ==");
+  modalEl.style.display = "none";
 }
 
 
 
 // ====== Modal Exit ======
-// span.onclick = function() {
-//   console.log("Button Exit");
-//     modal.style.display = "none";
-
-// window.onclick = function(event) {
-//   console.log("Window Exit");
-//     if (event.target == {
-//         modal.style.display = "none";
-//     };
-// }
