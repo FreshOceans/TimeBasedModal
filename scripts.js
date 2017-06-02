abccbb4726ebd6d28fd182839a16404c2be86e7c
FreshOceans <mperezz398@gmail.com>
2017-06-02 11:54:05 -0400

    inital commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// var modal = document.getElementById("primeModal");
+//
+// // var span = document.getElementsByClassName("close")[0];
+//
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     };
+// }
